218a01dca4ec6b3d98fa5b6ee595540d86063142
Marcos Rivas <marcosrivasr@gmail.com>
2023-12-19 17:49:48 -0600

    patron hoc terminado

--- 05-patrones/src/App.tsx
+++ 05-patrones/src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import Compound from "./patrones/compound/Compound";
 import Provider from "./patrones/provider/Provider";
+import Hoc from "./patrones/hoc/index";
 
 function App() {
-	return (
-		<div>
-			<Provider />
-			<Compound />
-		</div>
-	);
+  return (
+    <div>
+      <Hoc />
+      <Provider />
+      <Compound />
+    </div>
+  );
 }
 
 export default App;
--- 05-patrones/src/patrones/hoc/index.tsx
+++ 05-patrones/src/patrones/hoc/index.tsx
@@ -1,30 +1,24 @@
-import { useState, useEffect, ComponentType } from "react";
+import loadData from "./loadData";
 
-export default function loadData<T>(Component: ComponentType<T>, url: string) {
-	return function (props: Omit<T, "data">) {
-		const [data, setData] = useState(null);
-		const [loading, setLoading] = useState(true);
-		const [error, setError] = useState(null);
-		useEffect(() => {
-			fetch(url)
-				.then((res) => res.json())
-				.then((data) => {
-					setData(data);
-					setLoading(false);
-				})
-				.catch((err) => {
-					console.error(err);
-					setError(err);
-					setLoading(false);
-				});
-		}, []);
+interface TodosComponentProps {
+  data: any;
+}
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-		if (loading) {
-			return <div>Loading...</div>;
-		}
-		if (error) {
-			return <div>Error: {error}</div>;
-		}
-		return <Component data={data} {...(props as T)} />;
-	};
+function TodosComponent({ data }: TodosComponentProps) {
+  return (data as Todo[]).map((item) => <div key={item.id}>{item.title}</div>);
+}
+
+export default function Hoc() {
+  const Todos = loadData(
+    TodosComponent,
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+
+  return <Todos />;
 }
